7bf91951eb6f9ad0e9ba87e2cf3f4d0f590ef7b3
Asi <145265104+Asijain019@users.noreply.github.com>
2023-11-27 11:54:11 +0530

    Add files via upload

---
+++ lab5q.3(while).py
@@ -0,0 +1,33 @@
+n=int(input("enter:"))
+i=0
+while i<=n:
+    print('*'*i)
+    i+=1
+print()
+
+# solution for part b:
+n=int(input("enter:"))
+i=1
+while i<=n:
+    print(' '*(n-i) +'*'*i)
+    i+=1
+print()
+ #solution for c part:
+n=int(input("enter:"))
+i=1
+while i<=n:
+    print(' '*(n-i) +'*'*(2*i-1) )
+    i+=1
+print()
+
+#solution for question 7:
+n=int(input("enter:"))
+i=1
+while i<=n:
+    print(' '*(n-i) +'* '*i)
+    i+=1
+print()
+
+
+
+
